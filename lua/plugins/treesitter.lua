5bab09022443678effd628da007500257e7ba61d
ham-munculus <242454600+ham-munculus@users.noreply.github.com>
2026-05-28 10:42:08 -0700

    treesitter cleanup for linux

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,18 +3,30 @@ local ts = require("nvim-treesitter")
 ts.setup()
 
 local parsers = {
-  "python",
-  "lua",
-  "rust",
-  "sql",
-  "bash",
+	"python",
+	"lua",
+	"rust",
+	"sql",
+	"bash",
 }
 
 ts.install(parsers)
 
+local disabled = {
+	vim = true,
+	vimdoc = true,
+	help = true,
+}
+
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = parsers,
-  callback = function()
-    vim.treesitter.start()
-  end,
+	pattern = parsers,
+	callback = function(args)
+		local ft = vim.bo[args.buf].filetype
+
+		if disabled[ft] then
+			return
+		end
+
+		pcall(vim.treesitter.start, args.buf)
+	end,
 })

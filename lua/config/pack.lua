f867933c191b6ef2dcbc29a0d5eb896939f778ee
Andrew Richard <andrewrichard-strata@users.noreply.github.com>
2026-06-05 10:10:23 -0700

    added render-markdown

--- lua/config/pack.lua
+++ lua/config/pack.lua
@@ -14,6 +14,7 @@ vim.pack.add({
 	{ src = "https://github.com/j-hui/fidget.nvim.git" },
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
+	{ src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
 
 	-- formatting & editor
 	{ src = "https://github.com/stevearc/conform.nvim" },
